8021f62f0108ef2cda18b0ae919d767950ae4c29
dishasah <dishasah6@gmail.com>
2024-06-28 18:25:54 +0530

    hello2

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import CompletedTask from './components/CompletedTask';
+import TaskList from './components/TaskList';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App: React.FC = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+      <div className="todo_app_section">
+      <div className="todo_app_container">
+        <div className="new_task_list task_list">
+          <h2>Task List</h2>
+          <div><TaskList/></div>
+        </div>
+        <div className="completed_task_list task_list">
+          <h2>Completed Task</h2>
+          <div><CompletedTask/></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      </div>
+    </>
+  );
+
+};
+
+export default App;
